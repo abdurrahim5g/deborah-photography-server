bfe7afc92791bc3c614cb8e5e1ab687e45b04dbd
f-rahim <freelancerrahim26@gmail.com>
2023-06-19 19:52:04 +0600

    Connect mongoDB & Added services API to retrive services data from the database

--- index.js
+++ index.js
@@ -2,10 +2,39 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.undypbz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const services = client.db("photography").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const curser = services.find(query);
+      const result = await curser.toArray();
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Base get API
 app.get("/", (req, res) => {
