7b2412f9558c68254da5b68ada5afdd51c26d749
f-rahim <freelancerrahim26@gmail.com>
2023-06-20 21:21:28 +0600

    Added delete api

--- index.js
+++ index.js
@@ -55,6 +55,14 @@ async function run() {
       const result = await reviews.find(query, opitons).toArray();
       res.send(result);
     });
+
+    // delete review
+    app.delete("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await reviews.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
